82debaf27d8628dc78fa288f5b5e85de32253533
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-08-07 14:41:42 +0100

    +Delete button without the functionality

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,26 @@
 /* eslint-disable react/prop-types */
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
 import React from 'react';
 
 export default function Sidebar(props) {
-  
-
-  const noteElements = props.notes.map((note, index) =>
+  const noteElements = props.notes.map((note, index) => (
     <div key={note.id}>
       <div
-        className={`title ${note.id === props.currentNote.id ? 'selected-note' : ''
-          }`}
+        className={`title ${
+          note.id === props.currentNote.id ? 'selected-note' : ''
+        }`}
         onClick={() => props.setCurrentNoteId(note.id)}
       >
-        <h4 className="text-snippet">
-          {note.body.split('\n')[0] || `Note ${index + 1}`}
-        </h4>
+        <h4 className="text-snippet">{note.body.split('\n')[0]}</h4>
+        <button
+          className="delete-btn"
+          // Your onClick event handler here
+        >
+          <i className="gg-trash trash-icon"></i>
+        </button>
       </div>
     </div>
-  );
+  ));
 
   return (
     <section className="pane sidebar">
